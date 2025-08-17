5ac9cb39769ccfb9fdc45bc3cd4213095a6fa6c1
zingrin <jerinloncheu@gmail.com>
2025-08-18 03:36:14 +0600

    apitalize First Letter of Each Word

--- index.js
+++ index.js
@@ -82,7 +82,16 @@ console.log("Problem 7:", findEven([1, 2, 3, 4, 5, 6]));
 
 // Problem 8: Capitalize First Letter of Each Word
 
-
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+  for (let word of words) {
+    let capitalized = word[0].toUpperCase() + word.slice(1);
+    result.push(capitalized);
+  }
+  return result.join(" ");
+}
+console.log("Problem 8:", capitalizeWords("hello world"));
 // Problem 9: Find the Factorial of a Number
 
 
