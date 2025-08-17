4c53ca8eb5130c50200845bc780023f3fb31e915
zingrin <jerinloncheu@gmail.com>
2025-08-18 03:29:14 +0600

    slove problems  Count Vowels in a String

--- index.js
+++ index.js
@@ -25,9 +25,26 @@ console.log("Problem 2:", countVowels("programming"));
 
 // Problem 3: Check for Palindrome
 
+function isPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+console.log("Problem 3:", isPalindrome("madam")); 
+console.log("Problem 3:", isPalindrome("hello")); 
 
 
 // Problem 4: Find the Maximum Number
+function findMax(arr) {
+  let max = arr[0];
+  for (let num of arr) {
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max;
+}
+console.log("Problem 4:", findMax([5, 1, 9, 3])); 
+
 
 
 
