f08bd3d6305fd6770dac751bb588842bdfd13f6f
zingrin <jerinloncheu@gmail.com>
2025-08-18 03:06:20 +0600

    Reverse a String sloving

---
+++ index.js
@@ -0,0 +1,38 @@
+// Problem 1: Reverse a String
+
+function reverseString(str) {
+let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+console.log("Problem 1:", reverseString("hello"));
+// Problem 2: Count Vowels in a String
+
+
+
+// Problem 3: Check for Palindrome
+
+
+
+// Problem 4: Find the Maximum Number
+
+
+
+// Problem 5: Remove Duplicates from an Array
+
+// Problem 6: Sum of All Numbers in an Array
+
+
+
+// Problem 7: Find Even Numbers in an Array
+
+
+// Problem 8: Capitalize First Letter of Each Word
+
+
+// Problem 9: Find the Factorial of a Number
+
+
+// Problem 10: PingPong Challenge
