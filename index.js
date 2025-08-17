810afb1d44bfcdf72ddef5f80148a9d89ca27e80
zingrin <jerinloncheu@gmail.com>
2025-08-18 03:20:21 +0600

    add solution for Problem 2 - Count Vowels in a Strin

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+// Problem 1: Reverse a String
 
+function reverseString(str) {
+let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+console.log("Problem 1:", reverseString("hello"));
 // Problem 2: Count Vowels in a String
 
-
+function countVowels(str) {
+let vowels = "aeiou";
+  let count = 0;
+  str = str.toLowerCase();
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log("Problem 2:", countVowels("programming")); 
 
 // Problem 3: Check for Palindrome
 
@@ -14,7 +34,6 @@
 // Problem 5: Remove Duplicates from an Array
 
 // Problem 6: Sum of All Numbers in an Array
-
 
 
 // Problem 7: Find Even Numbers in an Array
