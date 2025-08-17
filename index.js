899f2890a71dacc490a3af293ec0f2071dae4e3a
zingrin <jerinloncheu@gmail.com>
2025-08-18 03:13:28 +0600

    add solution for Problem 1 - Reverse a String

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-// Problem 1: Reverse a String
 
-function reverseString(str) {
-let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return reversed;
-}
-console.log("Problem 1:", reverseString("hello"));
 // Problem 2: Count Vowels in a String
 
 
